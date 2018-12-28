01e6eaee748c9e903c853e12894ff3770178db91
Stamatis Tsolakidis <stamatistsolakidis@gmail.com>
2018-12-28 09:43:59 +0200

    UI fixes

--- src/containers/Loading.js
+++ src/containers/Loading.js
@@ -4,14 +4,14 @@ import { connect } from 'react-redux'
 // Loading spinner visual for SearchBar
 let Loading = ({ loading }) => (
     loading ?
-    <div>
-       <p>LOADING</p>
+    <div className="Loading-Element">
+       <p>LOADING...</p>
     </div> :
     null
     );
 
 // Take the loading value from the state and connect container
-const mapStateToProps = (state) => ({loading: state.loading})
+const mapStateToProps = (state) => ({loading: state.weatherReducer.loading})
 Loading = connect(mapStateToProps,null)(Loading)
 
 export default Loading;
